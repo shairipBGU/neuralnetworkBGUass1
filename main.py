soft_max_regression - implemented with overflow fix

main.py
@@ -1,9 +1,23 @@
-# receives w - all weights (in matrix form?), j - the label we want to check, x - the sample,
-# l - number of categories/labels
+import math
+import numpy as np
+
+
+# receives w - all weights (in matrix form?), j - the label we want to check zero based, x - the sample,
 # returns a probability of how much the classifier is sure the X sample label(y) is j  between 0 - 1 for the sample
-def soft_max_regression(j, x, w, l):
-    # todo implement and don't forget the trick to cancel out the infinity in exp
-    pass
+def soft_max_regression(j_category, x_sample, weights_matrix):
+    l_categories = len(weights_matrix[0])
+    max_xtw_value = 0
+    for i in range(l_categories):
+        max_temp = np.transpose(x_sample) @ weights_matrix[:, [i]]
+        if max_temp > max_xtw_value:
+            max_xtw_value = max_temp
+
+    numerator = math.exp(np.transpose(x_sample) @ weights_matrix[:, [j_category]] - max_xtw_value)
+    denominator = 0
+    for i in range(l_categories):
+        denominator += math.exp(np.transpose(x_sample) @ weights_matrix[:, [i]] - max_xtw_value)
+
+    return numerator / denominator
 
 
 # receives w - all weights (in matrix form?), c - hot one vectors, x - all of the samples in matrix form,
@@ -23,6 +37,16 @@ def soft_max_gradient_of_weights(m, x, w, c, p):
 
 
 def main():
+    weights = np.asarray([[1, 2, 3],
+                          [4, 5, 6],
+                          [7, 8, 9]])
+
+    x_sample = np.asarray([2, 2, 2])
+    j_category = 1
+
+    print(round(soft_max_regression(j_category, x_sample, weights), 4))
+
+
     # todo 1.1
     # todo write soft_max_loss
     # todo write soft_max_gradient_of_weights
@@ -35,7 +59,6 @@ def main():
 
     # todo 1.3
     # use SGD on the softmax example, with graphs after each epoch...
-    pass
 
 
 if __name__ == '__main__':